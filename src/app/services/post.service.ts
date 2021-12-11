fa0d0c06189ba2aaebbaeba6cf95b36dd2c3a740
celestin <46505055+celestin98@users.noreply.github.com>
2021-12-11 09:34:24 +0100

    update database

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -11,18 +11,18 @@ export class PostService {
   }
 
   fetchPosts() {
-    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/celestin98/lNgxs/postsDb/');
+    return this.http.get<Post[]>('https://my-json-server.typicode.com/celestin98/lNgxs/postsDb');
   }
 
   deletePost(id: number) {
-    return this.http.delete(`https://jsonplaceholder.typicode.com/celestin98/lNgxs/postsDb/${id}`);
+    return this.http.delete(`https://my-json-server.typicode.com/celestin98/lNgxs/postsDb/${id}`);
   }
 
   addPost(payload: Post) {
-    return this.http.post<Post>('https://jsonplaceholder.typicode.com/celestin98/lNgxs/posts/', payload);
+    return this.http.post<Post>('https://my-json-server.typicode.com/celestin98/lNgxs/postsDb/', payload);
   }
 
   updatePost(payload: Post, id: number) {
-    return this.http.put<Post>(`https://jsonplaceholder.typicode.com/celestin98/lNgxs/posts/${id}`, payload);
+    return this.http.put<Post>(`https://my-json-server.typicode.com/celestin98/lNgxs/postsDb/${id}`, payload);
   }
 }
