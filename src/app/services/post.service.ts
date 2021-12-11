022561fa1a642e3948bc5a825e244cc97e98aeee
celestin <46505055+celestin98@users.noreply.github.com>
2021-12-11 09:17:57 +0100

    correct

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -11,18 +11,18 @@ export class PostService {
   }
 
   fetchPosts() {
-    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/celestin98/lNgxs/postsDb/');
   }
 
   deletePost(id: number) {
-    return this.http.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    return this.http.delete(`https://jsonplaceholder.typicode.com/celestin98/lNgxs/postsDb/${id}`);
   }
 
   addPost(payload: Post) {
-    return this.http.post<Post>('https://jsonplaceholder.typicode.com/posts', payload);
+    return this.http.post<Post>('https://jsonplaceholder.typicode.com/celestin98/lNgxs/posts/', payload);
   }
 
   updatePost(payload: Post, id: number) {
-    return this.http.put<Post>(`https://jsonplaceholder.typicode.com/posts/${id}`, payload);
+    return this.http.put<Post>(`https://jsonplaceholder.typicode.com/celestin98/lNgxs/posts/${id}`, payload);
   }
 }
